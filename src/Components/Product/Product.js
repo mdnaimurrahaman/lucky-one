46f324f024f8eaf872feea86edacd9f58f993498
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-03-26 11:41:40 +0600

    product info done

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './Product.css'
 
 const Product = ({product}) => {
-    
+    const {name,image,price} = product
+    return (
+        <div>
+            <img src={image} alt="" srcset="" />
+            <div>
+                <h3>{name}</h3>
+                <h5>${price}</h5>
+            </div>
+            <button className='btn-card'>Add to card</button>
+        </div>
+    );
 };
 
 export default Product;
