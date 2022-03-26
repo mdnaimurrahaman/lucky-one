6d3413cdbeb705ed192a54fb409ce4e3456c117a
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-03-26 11:39:05 +0600

    all products done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../img/2756308.png'
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <img src={logo} alt="" srcset="" />
+        </div>
+    );
+};
+
+export default Header;
