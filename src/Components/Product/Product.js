89022592901b43d6c8fc4bc83584e876fd4015c3
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-03-26 13:13:17 +0600

    cart style done

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,16 +1,21 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Product.css'
 
 const Product = ({product}) => {
     const {name,image,price} = product
     return (
-        <div>
+        <div className='product'>
             <img src={image} alt="" srcset="" />
-            <div>
+            <div className='product-info'>
                 <h3>{name}</h3>
-                <h5>${price}</h5>
+                <h5 className=''>${price}</h5>
             </div>
-            <button className='btn-card'>Add to card</button>
+            <div className='cart-btn'>
+                <button className='btn-cart'>Add to card <FontAwesomeIcon className='icon' icon={faShoppingCart}></FontAwesomeIcon>
+                </button>
+            </div>
         </div>
     );
 };
